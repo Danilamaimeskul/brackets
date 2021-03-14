4dc62c2e78ab456f3373e3a352836ce837d6a652
Danilamaimeskul <dan.maika@mail.ru>
2021-03-14 10:27:11 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = [];
+  let existBracket = {};
+  for(let j in bracketsConfig) {
+    existBracket[bracketsConfig[j][0]] = bracketsConfig[j][1];
+  }
+
+  result.push(str.charAt(0));
+
+  for(let i = 1; i < str.length; i += 1) {
+    if(str.charAt(i) === existBracket[result[result.length - 1]]) {
+      result.pop();
+    } else {
+      result.push(str.charAt(i)); 
+    }
+  }
+  if (result.length > 0) return false;
+  else return true;
 }
